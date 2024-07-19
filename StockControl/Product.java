e83d82dd7aadda6ca187914954439382177dc090
Camilo1599 <camimilo2014@hotmail.com>
2024-07-18 21:08:00 -0500

    ya vamos terminando

--- StockControl/Product.java
+++ StockControl/Product.java
@@ -1,5 +1,35 @@
 package StockControl;
 
 public class Product {
+    int id;
+    String description;
+    int stockQuantity;
+
+    public Product(int id, String description, int stockQuantity){
+        this.id = id;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
